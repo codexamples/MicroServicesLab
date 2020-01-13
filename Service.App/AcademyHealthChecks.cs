7dfa1c1f3a4d251258676f228774bec6333b3765
Khalid Awwad <khaw@microsoft.com>
2020-01-13 08:42:01 +0200

    Adding .net core 3.1 health checks

---
+++ Service.App/AcademyHealthChecks.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="AcademyHealthChecks.cs" company="Microsoft">
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// </copyright>
+
+namespace Service.App
+{
+    using Microsoft.Extensions.Diagnostics.HealthChecks;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class AcademyHealthChecks : IHealthCheck
+    {
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult(HealthCheckResult.Unhealthy("SQL doesn't connect"));
+        }
+    }
+}
